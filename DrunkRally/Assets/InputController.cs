c5e75ff2d11cef92976a0c25315e9170c90f7c01
mathek <front@student.agh.edu.pl>
2018-03-22 19:25:41 +0100

    initial game project

---
+++ DrunkRally/Assets/InputController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rb.AddForce (-transform.up * 1000);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey ("w")) {
+			rb.AddForce (transform.forward*20000);
+//			transform.Translate(new Vector3 (0, 0, 1));
+		}
+		if (Input.GetKey ("s")) {
+			rb.AddForce (-transform.forward*20000);
+		}
+		if (Input.GetKey ("a")) {
+			transform.Rotate(new Vector3 (0, (float)-0.5, 0));
+		}
+		if (Input.GetKey ("d")) {
+			transform.Rotate(new Vector3 (0, (float)0.5, 0));
+		}
+	}
+}
