e5be0d38e2955de34ac93bc35012963b4cf17795
mathek <front@student.agh.edu.pl>
2018-05-30 12:15:18 +0200

    added startline smoke

--- DrunkRally/Assets/Timer.cs
+++ DrunkRally/Assets/Timer.cs
@@ -6,21 +6,24 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
     public Text timerText;
     private float startTime;
+    private bool is_started = false;
 	// Use this for initialization
 	void Start () {
-		StartLap();
+		//StartLap();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (!is_started) return;
 		float dt = Time.time - startTime;
-        string minutes = ((int) dt / 60).ToString();
-        string seconds = (dt % 60).ToString("f2");
+        string minutes = ((int)dt / 60).ToString();
+        string seconds = (dt % 60).ToString("f2").PadLeft(5, '0');
 
         timerText.text = minutes + ":" + seconds; 
 	}
 
     public void StartLap() {
         startTime = Time.time;
+        is_started = true;
     }
 }
